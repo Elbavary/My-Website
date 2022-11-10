cd6a9b9492043d08febbd70f8713638bf24feaa1
Khaled <khaledgamel2012@gmail.com>
2022-11-10 17:28:20 +0200

    Initial commit

---
+++ src/container/Skills/skills.js
@@ -0,0 +1,44 @@
+import images from '../../constants';
+
+export const skills = [
+  {
+    name: 'Html5',
+    icon: images.html,
+  },
+  {
+    name: 'CSS3',
+    icon: images.css,
+  },
+  {
+    name: 'JavaScript',
+    icon: images.javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: images.typescript,
+  },
+  {
+    name: 'SASS',
+    icon: images.sass,
+  },
+  {
+    name: 'Bootstrap',
+    icon: images.bootstrap,
+  },
+  {
+    name: 'Tailwind',
+    icon: images.tailwind,
+  },
+  {
+    name: 'React',
+    icon: images.react,
+  },
+  {
+    name: 'Redux',
+    icon: images.redux,
+  },
+  {
+    name: 'Git',
+    icon: images.git,
+  },
+];
---
+++ src/container/Work/works.js
@@ -0,0 +1,28 @@
+import images from '../../constants';
+
+export const works = [
+  {
+    title: 'Web Development',
+    description: 'I Am A Good Web Developer',
+    imgUrl: images.about01,
+    tags: ['UI/UX', 'ALL'],
+  },
+  {
+    title: 'Web Design',
+    description: 'I Am A Good Web Developer',
+    imgUrl: images.about02,
+    tags: ['UI/UX', 'ALL'],
+  },
+  {
+    title: 'UI/UX',
+    description: 'I Am A Good Web Developer',
+    imgUrl: images.about03,
+    tags: ['Mobile App', 'ALL'],
+  },
+  {
+    title: 'Web Animations',
+    description: 'I Am A Good Web Developer',
+    imgUrl: images.about04,
+    tags: ['Mobile App', 'ALL'],
+  },
+];
